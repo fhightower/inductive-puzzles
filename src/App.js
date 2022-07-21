c7b4364272b16e51091c3f5ee1d7f32dced33de7
Floyd Hightower <floyd.hightower27@gmail.com>
2022-07-21 10:43:55 -0400

    Add level1

--- src/App.js
+++ src/App.js
@@ -1,17 +1,27 @@
 import React from 'react';
 import './App.css';
 import Timer from './components/Timer/Timer';
+import Level1 from './components/Levels/Level1';
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {started: false};
+    this.onStart = this.onStart.bind(this);
   }
+
+  onStart(e) {
+    console.log("here!")
+    this.setState(state => ({
+      started: true
+    }));
+  };
 
   render() {
     return (
       <div>
-	<Timer />
+	<Timer onChange={this.onStart} />
+	<span>Started: {this.state.started}</span>
       </div>
     );
   }
