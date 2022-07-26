62b816fe5ec9a9890c827da721fb9f64fa9c8a63
Floyd Hightower <floyd.hightower27@gmail.com>
2022-07-26 10:39:02 -0400

    Add new level

---
+++ src/components/Levels/Level2.js
@@ -0,0 +1,46 @@
+import React from 'react';
+
+class Level2 extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: 0,
+      target: 13,
+    };
+  };
+
+  checkValue() {
+    if (this.state.value === this.state.target) {
+      this.setState({
+	      value: this.state.value + "- Done!"
+      }, this.props.completeLevel);
+    }
+  }
+
+  incEven = e => {
+    this.setState({
+      value: this.state.value + 1
+    }, this.checkValue);
+  }
+
+  dec = e => {
+    this.setState({
+      value: this.state.value - 1
+    }, this.checkValue);
+  }
+
+  render() {
+    return (
+	<div>{(this.props.parentStarted && (this.props.level === 2))?
+		<div>
+			<button onClick={this.incEven}>a</button>
+			<button onClick={this.dec}>b</button>
+			<p>Current Value: {this.state.value}</p>
+			<p>Target: {this.state.target}</p>
+		</div>
+		: ""}</div>
+    )};
+}
+
+export default Level2;
+
