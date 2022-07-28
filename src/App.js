49440d6ddaa57d0fdfa280198e8d417cc4e5a538
Floyd Hightower <floyd.hightower27@gmail.com>
2022-07-28 10:46:41 -0400

    Moving timer into main app

--- src/App.js
+++ src/App.js
@@ -1,44 +1,62 @@
 import React from 'react';
 import './App.css';
-import Timer from './components/Timer/Timer';
 import Level1 from './components/Levels/Level1';
 import Level2 from './components/Levels/Level2';
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {started: false, level: 1};
-    this.onStart = this.onStart.bind(this);
-    this.completeLevel = this.completeLevel.bind(this);
-	this.timerStop = React.createRef();
-  }
+	constructor(props) {
+		super(props);
+		this.state = {started: false, level: 1, seconds: 0};
+		this.completeLevel = this.completeLevel.bind(this);
+		this.startTimer = this.startTimer.bind(this);
+	}
 
-  onStart(value) {
-    this.setState({
-      started: value
-    });
-  };
+	incTimer() {
+		this.setState({
+			seconds: this.state.seconds + 1
+		});
+	}
+
+	startTimer(e) {
+		e.preventDefault();
+		this.interval = setInterval(() => this.incTimer(), 1000);
+		this.setState({
+			started: true
+		});
+	}
+
+	stopTimer() {
+		clearInterval(this.interval);
+	}
+
+	componentWillUnmount() {
+		clearInterval(this.interval);
+	}
 
 	completeLevel() {
 		// stop timer
-		this.timerStop.current.stopTimer();
+		this.stopTimer();
 		// show option for next level
 		this.setState({
-			level: this.state.level + 1
+			level: this.state.level + 1,
+			started: false
 		});
 		console.log("start next level...");
 	}
 
-  render() {
-    return (
-      <div>
-		<p>Level: {this.state.level}</p>
-	<Timer onChange={this.onStart} ref={this.timerStop} />
-	<Level1 level={this.state.level} parentStarted={this.state.started} completeLevel={this.completeLevel}/>
-	<Level2 level={this.state.level} parentStarted={this.state.started} completeLevel={this.completeLevel}/>
-      </div>
-    );
-  }
+	render() {
+		return (
+			<div>
+			<p>Level: {this.state.level}</p>
+			<form onSubmit={this.startTimer}>
+			<button>Start</button>
+			</form>
+			{this.state.started ? <p>Seconds: {this.state.seconds}</p>: "Click the button to get started..."}
+			<Level1 level={this.state.level} parentStarted={this.state.started} completeLevel={this.completeLevel}/>
+			<Level2 level={this.state.level} parentStarted={this.state.started} completeLevel={this.completeLevel}/>
+			</div>
+		);
+	}
 }
 
 export default App;
